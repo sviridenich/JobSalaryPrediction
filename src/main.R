7c20991a62e7935f685849b8f01bacabce36d9b9
Sergey Sviridenko <Sergey.Sviridenko@gmail.com>
2013-04-28 11:47:43 +0300

    Just supporting wordProcessing routine

--- src/main.R
+++ src/main.R
@@ -8,7 +8,17 @@ require("ProjectTemplate")
 load.project()
 
 # data used in the model
-save( list = ls(pattern="DTB"), file='~/cache/DTB.RData' )
-load(file='~/cache/DTB.RData')
 
+save( list = ls(pattern="DTB"), file=paste( getwd(),'/cache/DTB.RData',sep='') )
+load(file=paste( getwd(),'/cache/DTB.RData',sep=''))
 
+# corpus used in the model
+
+save( list = ls(pattern="Corpora"), file=paste( getwd(),'/cache/Corpora.RData',sep='') )
+load(file=paste( getwd(),'/cache/Corpora.RData',sep=''))
+
+# small subset to get my procedure tested
+set.seed(123234)
+ind <- sample.int(n = dim(Train_DTB)[1], size = 1000, replace = FALSE)
+Train_DTB_small <- Train_DTB[ind,]
+DTB <- Train_DTB_small
